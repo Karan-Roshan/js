9dd683726fdc7ca83320f5f753d033f268201b63
Karan Roshan <karanroshan91@gmail.com>
2026-02-07 19:53:22 +0530

    update variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,3 @@
-/*
 const accountID = 12345;
 let accountEmail = "karan@gmail.com";
 var accountPassword = "karan@12345";
@@ -11,44 +10,30 @@ accountCity = "jharkhand"
 
 console.log(accountID);
 console.table([accountID, accountEmail, accountPassword, accountCity])
-*/
 
 
-/*
+
+
 // ------ var -----
 // Hoisting Use Cases
-console.log(a);     // undefined
+// console.log(a);     // undefined
 var a;
 var a = 44;
-console.log(a);
+// console.log(a);
 
 // Function Scope Behavior
 function test() {
   var x = 10;
 }
 // console.log(x); // Error
-*/
 
 
-/*
+
+
 // ------ let -----
 let b;
-console.log(b);
+// console.log(b);
 // let b = 55;      // not allowed
 b = 55;
-console.log(b);
-*/
+// console.log(b);
 
-
-
-// function myFun() {
-//     var a = 23;
-//     function myFun2() {
-//         console.log(++a);
-//         function myFun3() {
-//             console.log(++a);
-//         }
-//     }
-// }
-
-// console.log(a);
