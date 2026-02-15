abb523bf8afc6d893332b25eb8a839af7b629f84
Karan Roshan <karanroshan91@gmail.com>
2026-02-15 15:27:04 +0530

    do while Loop

---
+++ 05_iterations/doWhileLoop.js
@@ -0,0 +1,18 @@
+// ----- Do - while Loop -----
+
+let score = 1;
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+
+let num = 11
+
+do {
+    console.log(`Score is ${num}`);
+    score++
+} while (score <= 10);
