088443e793024619a20d5de2fe301912ca8365ad
Karan Roshan <karanroshan91@gmail.com>
2026-02-15 16:06:21 +0530

    forin Loop

---
+++ 05_iterations/forinLoop.js
@@ -0,0 +1,41 @@
+// for in Loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);   
+}
+
+
+
+
+// Note: Maps in not iterable
+/*
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
+*/
